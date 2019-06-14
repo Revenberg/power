82cb4e4068352094f7bd6cfcc5bb56c792739ac1
Revenberg <sander@revenberg.info>
2019-06-14 15:26:13 +0200

    Update logger.py

--- roles/logger/files/logger.py
+++ roles/logger/files/logger.py
@@ -200,7 +200,6 @@ def send_to_influxdb(options, fields):
     client = InfluxDBClient(options.influx_hostname, options.influx_port, options.influx_username, options.influx_password, options.influx_database)
     client.write_points(reqs, retention_policy=options.influx_retention_policy, database=options.influx_database)
 
-
 def start_monitor(options):
 
     meter = SmartMeter(options.device, options.baudrate)
