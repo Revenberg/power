4082354a654e40be69974afa53ff010accd9381b
Sander Revenberg <sander@revenberg.info>
2020-08-21 13:42:39 +0200

    .

--- roles/openweathermap/files/weather-logger.py
+++ roles/openweathermap/files/weather-logger.py
@@ -15,40 +15,95 @@ do_raw_log = config.getboolean('Logging', 'do_raw_log')
 apikey = config.get('Weather', 'apikey')
 country = config.get('Weather', 'country')
 language = config.get('Weather', 'language')
-# You MUST provide a valid API key
 
-owm = pyowm.OWM(apikey, language=language)
-# Here put your city and Country ISO 3166 country codes
-observation = owm.weather_at_place(country) 
+influx_server = config.get('InfluxDB', 'influx_server')
+influx_port = int(config.get('InfluxDB', 'influx_port'))
+influx_database = config.get('InfluxDB', 'database')
+influx_measurement = config.get('InfluxDB', 'measurement')
 
-w = observation.get_weather()
-# Weather details from INTERNET 
+if __debug__:
+    print("running with debug")
+    print(listen_address)
+    print(listen_port)
+    print(influx_server)
+    print(influx_port)
+    print(influx_database)
+    print(influx_measurement)
+    print(log_path)
+    print(do_raw_log)
+else:
+    print("running without debug")    
 
-status = w.get_status()         # short version of status (eg. 'Rain')
-detailed_status = w.get_detailed_status()  # detailed version of status (eg. 'light rain')
+# if the db is not found, then try to create it
+try:
+    dbclient = InfluxDBClient(host=influx_server, port=influx_port)
+    dblist = dbclient.get_list_database()
+    db_found = False
+    for db in dblist:
+        if db['name'] == influx_database:
+            db_found = True
+    if not(db_found):
+        print('Database ' + influx_database + ' not found, trying to create it')
+        dbclient.create_database(influx_database)
+        dbclient.create_retention_policy('30_days', '30d', 1, default=True)
+        dbclient.create_retention_policy('6_months', '26wd', 1, default=False)
+        dbclient.create_retention_policy('infinite', 'INF', 1, default=False)
 
-wind = w.get_wind() 
-speed = wind ["speed"] 
-deg = wind ["deg"] 
-humidity = w.get_humidity() 
-temperature = w.get_temperature('celsius') 
-temp = temperature["temp"] 
-rain = w.get_rain() 
-pressure = w.get_pressure()['press']
+except Exception as e:
+    print('Error querying open database: ' + e)
 
-#If there is no data recorded from rain then return 0, otherwise #return the actual data
-if len(rain) == 0: 
-      lastrain = 0 
-else: 
-      lastrain = rain["3h"]
+try:
+    while True:    
+        owm = pyowm.OWM(apikey, language=language)
+        # Here put your city and Country ISO 3166 country codes
+        observation = owm.weather_at_place(country) 
 
-# Print the data 
-print "The status is: " + detailed_status
-print "The detailed status is: " + detailed_status
-print "The velocity of the air is: " + str(speed) + " m/s" 
-print "The wind direction is: " + str(deg) + " Degrees" 
-print "The humidity is: " + str(humidity) + " %" 
-print "The temperature is: " + str(temp) + " Celcius" 
-print "The precipitation is: " + str(lastrain) + " mm"
-print "The pressure is: " + str(pressure) + " mbar"
+        w = observation.get_weather()
+        # Weather details from INTERNET 
 
+        values = dict()
+        values['status'] = w.get_status()         # short version of status (eg. 'Rain')
+        values['detailed_status']  = w.get_detailed_status()  # detailed version of status (eg. 'light rain')
+
+        values['wind']  = w.get_wind() 
+        values['speed']  = wind ["speed"] 
+        values['deg']  = wind ["deg"] 
+        values['humidity']  = w.get_humidity() 
+        values['temperature']  = w.get_temperature('celsius') 
+        values['temp']  = temperature["temp"] 
+        values['pressure'] = w.get_pressure()['press']
+
+        rain = w.get_rain() 
+        #If there is no data recorded from rain then return 0, otherwise #return the actual data
+        if len(rain) == 0: 
+            values['lastrain'] = 0 
+        else: 
+            values['lastrain'] = rain["3h"]
+
+        # Print the data 
+        print(values)
+        
+        json_body = {'points': [{'tags': 'fields': {k: v for k, v in values.items()}
+                                        }],
+                            'measurement': influx_measurement
+                            }
+
+                client = InfluxDBClient(host=influx_server,
+                                        port=influx_port)
+                success = client.write(json_body,
+                                    # params isneeded, otherwise error 'database is required' happens
+                                    params={'db': influx_database})
+
+                if not success:
+                    print('error writing to database')
+
+                client.close()
+
+        time.sleep( 300 )
+    except Exception as e:
+        print(e)
+        print("Unexpected error:", sys.exc_info()[0])
+#        raise
+    finally:
+        if __debug__:
+            print("Finally")
