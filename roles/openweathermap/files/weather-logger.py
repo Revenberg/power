9b6dc6c5fc6396761c744dfa5ba3a7cd2eacfbb1
Sander Revenberg <sander@revenberg.info>
2020-08-21 13:32:34 +0200

    .

--- roles/openweathermap/files/weather-logger.py
+++ roles/openweathermap/files/weather-logger.py
@@ -24,8 +24,8 @@ observation = owm.weather_at_place(country)
 w = observation.get_weather()
 # Weather details from INTERNET 
 
-w.status           # short version of status (eg. 'Rain')
-w.detailed_status  # detailed version of status (eg. 'light rain')
+status = w.get_status()         # short version of status (eg. 'Rain')
+detailed_status = w.get_detailed_status()  # detailed version of status (eg. 'light rain')
 
 wind = w.get_wind() 
 speed = wind ["speed"] 
@@ -34,6 +34,7 @@ humidity = w.get_humidity()
 temperature = w.get_temperature('celsius') 
 temp = temperature["temp"] 
 rain = w.get_rain() 
+pressure = w.get_pressure()['press']
 
 #If there is no data recorded from rain then return 0, otherwise #return the actual data
 if len(rain) == 0: 
@@ -41,16 +42,13 @@ if len(rain) == 0:
 else: 
       lastrain = rain["3h"]
 
-pressure = w.pressure['press']
-
-
 # Print the data 
-print "The status is: " + w.status
 print "The status is: " + detailed_status
+print "The detailed status is: " + detailed_status
 print "The velocity of the air is: " + str(speed) + " m/s" 
 print "The wind direction is: " + str(deg) + " Degrees" 
 print "The humidity is: " + str(humidity) + " %" 
 print "The temperature is: " + str(temp) + " Celcius" 
 print "The precipitation is: " + str(lastrain) + " mm"
-print "The pressure is: " + str(pressure) + " mm"
+print "The pressure is: " + str(pressure) + " mbar"
 
