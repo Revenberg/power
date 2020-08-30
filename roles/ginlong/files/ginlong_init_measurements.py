e5760182c37180b75bbf346e49e1148732a15021
Sander Revenberg <sander@revenberg.info>
2020-08-30 10:44:51 +0200

    ginlong

---
+++ roles/ginlong/files/ginlong_init_measurements.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3 -O
+
+import os
+import socket
+import binascii
+import time
+import sys
+import configparser
+from influxdb import InfluxDBClient
+
+config = configparser.RawConfigParser(allow_no_value=True)
+config.read("config.ini")
+
+###########################
+# Variables
+
+do_raw_log = config.getboolean('Logging', 'do_raw_log')
+
+influx_server = config.get('InfluxDB', 'influx_server')
+influx_port = int(config.get('InfluxDB', 'influx_port'))
+influx_database = config.get('InfluxDB', 'database')
+
+if __debug__:
+    print("running with debug")
+    print(influx_server)
+    print(influx_port)
+    print(influx_database)
+    print(do_raw_log)
+else:
+    print("running without debug")
+
+# if the db is not found, then try to create it
+try:
+    dbclient = InfluxDBClient(host=influx_server, port=influx_port)
+    dblist = dbclient.get_list_database()
+    db_found = False
+    for db in dblist:
+        if db['name'] == influx_database:
+            db_found = True
+    if not(db_found):
+        print('Database ' + influx_database + ' not found, trying to create it')
+        dbclient.create_database(influx_database)
+
+    dbclient.create_retention_policy('10_days', '10d', 1, influx_database, default=True)
+    dbclient.create_retention_policy('60_days', '60d', 1, influx_database, default=False)
+    dbclient.create_retention_policy('infinite', 'INF', 1, influx_database, default=False)
+
+    print( dbclient.get_list_continuous_queries() )
+
+#    ginlong_select_clause = 'SELECT mean("clouds") as "clouds",mean("detailed_status") as "detailed_status",mean("humidity") as "humidity",mean("lastrain") as "lastrain",mean("lastsnow") as "lastsnow",mean("location") as "location",mean("pressure") as "pressure",mean("status") as "status",mean("sunrise") as "sunrise",mean("sunset") as "sunset",mean("temp") as "temp",mean("ginlong_code") as "ginlong_code",mean("ginlong_icon") as "ginlong_icon",mean("wind_direction_deg") as "wind_direction_deg",mean("wind_speed") as "wind_speed"'
+#    dbclient.create_continuous_query("ginlong_mean60", ginlong_select_clause + ' INTO "60_days"."ginlong" FROM "ginlong" GROUP BY time(15m)', influx_database )
+    dbclient.create_continuous_query("ginlong_meaninf", ginlong_select_clause + ' INTO "infinite"."ginlong" FROM "ginlong" GROUP BY time(30m)', influx_database )
+
+
+    print( dbclient.get_list_continuous_queries() )
+
+except Exception as e:
+    print('Error querying open database: ' + influx_database)
+    print(e)
