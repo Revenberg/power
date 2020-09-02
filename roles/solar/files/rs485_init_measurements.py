44a8ad56d5ae548d87cbf617f32ff65fdf069784
Sander Revenberg <sander@revenberg.info>
2020-09-02 20:53:32 +0200

    .

--- roles/solar/files/rs485_init_measurements.py
+++ roles/solar/files/rs485_init_measurements.py
@@ -48,7 +48,7 @@ try:
     print( dbclient.get_list_continuous_queries() )
 
     rs485_select_clause = 'SELECT mean("AC Current 1 (A)") as "AC Current 1 (A)",mean("AC Current 2 (A)") as "AC Current 2 (A)",mean("AC Current 3 (A)") as "AC Current 3 (A)",mean("AC Frequency (Hz)") as "AC Frequency (Hz)",mean("AC Watts (W)") as "AC Watts (W)",mean("AC voltage 1 (V)") as "AC voltage 1 (V)",mean("AC voltage 2 (V)") as "AC voltage 2 (V)",mean("AC voltage 3 (V)") as "AC voltage 3 (V)",mean("DC Current 1 (A)") as "DC Current 1 (A)",mean("DC Current 2 (A)") as "DC Current 2 (A)",mean("DC Voltage 1 (V)") as "DC Voltage 1 (V)",mean("DC Voltage 2 (V)") as "DC Voltage 2 (V)",mean("Generated (All time)") as "Generated (All time)",mean("Generated (Today)") as "Generated (Today)",mean("Inverter Temperature (c)") as "Inverter Temperature (c)",mean("Last month energy (W)") as "Last month energy (W)",mean("Last year energy") as "Last year energy",mean("Month energy (W)") as "Month energy (W)",mean("Total energy (W)") as "Total energy (W)",mean("ac power (A)") as "ac power (A)",mean("pv power (V)") as "pv power (V)"'
-    dbclient.create_continuous_query("rs485_mean60", rs485_select_clause + ' INTO "60_days"."rs485" GROUP BY time(15m)', influx_database )
+    dbclient.create_continuous_query("rs485_mean60", rs485_select_clause + ' INTO "60_days"."rs485" FROM "rs485" GROUP BY time(15m)', influx_database )
     dbclient.create_continuous_query("rs485_meaninf", rs485_select_clause + ' INTO "infinite"."rs485" FROM "rs485" GROUP BY time(30m)', influx_database )
 
     print( dbclient.get_list_continuous_queries() )
