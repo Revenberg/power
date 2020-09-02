e95b67d9be04a664288de82eb08656704af9984b
Sander Revenberg <sander@revenberg.info>
2020-09-02 18:24:20 +0200

    .

--- roles/solar/files/solar_rs485_eth.py
+++ roles/solar/files/solar_rs485_eth.py
@@ -1,4 +1,4 @@
-import rs485-eth.py
+import rs485eth.py
 import socket
 import serial
 import pyowm
@@ -61,12 +61,12 @@ def getData():
     values['AC Frequency (Hz)'] = instrument.read_register(3042, functioncode=4, signed=False) / 100 #Read AC Frequency as Unsigned 16-Bit
     values['Inverter Temperature (c)'] = instrument.read_register(3041, functioncode=4, signed=True) / 10 #Read Inverter Temperature as Signed 16-B$
 
-    Realtime_DATA_yy'] = instrument.read_register(3072, functioncode=4, signed=False) #Read Year
-    Realtime_DATA_mm'] = instrument.read_register(3073, functioncode=4, signed=False) #Read Month
-    Realtime_DATA_dd'] = instrument.read_register(3074, functioncode=4, signed=False) #Read Day
-    Realtime_DATA_hh'] = instrument.read_register(3075, functioncode=4, signed=False) #Read Hour
-    Realtime_DATA_mi'] = instrument.read_register(3076, functioncode=4, signed=False) #Read Minute
-    Realtime_DATA_ss'] = instrument.read_register(3077, functioncode=4, signed=False) #Read Second
+    Realtime_DATA_yy = instrument.read_register(3072, functioncode=4, signed=False) #Read Year
+    Realtime_DATA_mm = instrument.read_register(3073, functioncode=4, signed=False) #Read Month
+    Realtime_DATA_dd = instrument.read_register(3074, functioncode=4, signed=False) #Read Day
+    Realtime_DATA_hh = instrument.read_register(3075, functioncode=4, signed=False) #Read Hour
+    Realtime_DATA_mi = instrument.read_register(3076, functioncode=4, signed=False) #Read Minute
+    Realtime_DATA_ss = instrument.read_register(3077, functioncode=4, signed=False) #Read Second
     
     values['ac power (A)'] = instrument.read_register(3005, functioncode=4, signed=False) #Read AC Frequency as Unsigned 16-Bit
     values['pv power (V)'] = instrument.read_register(3007, functioncode=4, signed=False) #Read AC Frequency as Unsigned 16-Bit
@@ -128,6 +128,7 @@ def openDatabase():
 
 try:
     while True:
+        openDatabase()
         getData()
         time.sleep( 30 )
 
